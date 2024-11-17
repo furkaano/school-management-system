abc836c7e24e593a45bf7bd8cbeb931566cae6e9
Furkan Öcalan <furkan1907903@gmail.com>
2024-11-17 11:01:29 +0300

    auth service structure is created

---
+++ auth-service/src/main/java/com/schoolmanagement/authservice/entity/User.java
@@ -0,0 +1,29 @@
+package com.schoolmanagement.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String role; // ROLE_USER, ROLE_ADMIN
+
+    @Column(nullable = false)
+    private boolean active = true;
+}
